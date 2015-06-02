39006cec0a0c2d71ff1fde4a93e91b114e2bff93
Mariusz Nowak <medyk@medikoo.com>
2015-06-02 15:41:33 +0200

    Move view resolution logic to tree
    So it can be customized

--- index.js
+++ index.js
@@ -2,32 +2,65 @@
 
 'use strict';
 
-var find           = require('es5-ext/array/#/find')
-  , includes       = require('es5-ext/array/#/contains')
-  , assign         = require('es5-ext/object/assign')
-  , forEach        = require('es5-ext/object/for-each')
-  , ensureCallable = require('es5-ext/object/valid-callable')
-  , ensureObject   = require('es5-ext/object/valid-object')
-  , ensureValue    = require('es5-ext/object/valid-value')
-  , partial        = require('es5-ext/function/#/partial')
-  , d              = require('d')
-  , ee             = require('event-emitter')
-  , memoizeMethods = require('memoizee/methods-plain')
-  , getNormalizer  = partial.call(require('memoizee/normalizers/get-fixed'), 2)
-  , ensureIdent    = require('dom-ext/element/ensure-ident')
-  , ensureDocument = require('dom-ext/html-document/valid-html-document')
-  , reflow         = require('dom-ext/html-document/#/reflow')
-  , resetForms     = require('html-dom-ext/element/#/reset-forms')
-  , SiteNode       = require('./lib/node');
+var find               = require('es5-ext/array/#/find')
+  , includes           = require('es5-ext/array/#/contains')
+  , assign             = require('es5-ext/object/assign')
+  , forEach            = require('es5-ext/object/for-each')
+  , ensureCallable     = require('es5-ext/object/valid-callable')
+  , ensureObject       = require('es5-ext/object/valid-object')
+  , ensureValue        = require('es5-ext/object/valid-value')
+  , partial            = require('es5-ext/function/#/partial')
+  , d                  = require('d')
+  , ee                 = require('event-emitter')
+  , memoizeMethods     = require('memoizee/methods-plain')
+  , getNormalizer      = partial.call(require('memoizee/normalizers/get-fixed'), 2)
+  , ensureIdent        = require('dom-ext/element/ensure-ident')
+  , ensureDocument     = require('dom-ext/html-document/valid-html-document')
+  , reflow             = require('dom-ext/html-document/#/reflow')
+  , resetForms         = require('html-dom-ext/element/#/reset-forms')
+  , SiteNode           = require('./lib/node')
+  , resolveRootElement = require('./lib/resolve-root-element')
+
+  , rootNames = { head: true, title: true, body: true }
+
+  , stringify = JSON.stringify;
 
 var SiteTree = module.exports = Object.defineProperties(function (document) {
 	if (!(this instanceof SiteTree)) return new SiteTree(document);
 	this.document = ensureDocument(document);
 }, {
-	// Default template function resolver, it should return documentFragment instance
+	// Default view resolver
+	// (this method may be overriden on subclasses for custom needs)
+	resolveView: d(function (document, conf, context) {
+		var map = [];
+		forEach(conf, function (setup, id) {
+			var conf;
+			if (id[0] === '_') return;
+			map.push(conf = {});
+			if (rootNames[id]) conf.element = resolveRootElement(document, id);
+			else conf.element = document.getElementById(id);
+			if (!conf.element) throw new TypeError("Could not find element of id " + stringify(id));
+			if (typeof setup === 'function') {
+				conf.content = this.resolveTemplate(setup, context);
+				return;
+			}
+			if (setup.class) conf.class = setup.class;
+			if (setup.content) {
+				conf.content = this.resolveTemplate(setup.content, context);
+			} else {
+				if (setup.prepend) conf.prepend = this.resolveTemplate(setup.prepend, context);
+				if (setup.append) conf.append = this.resolveTemplate(setup.append, context);
+			}
+		}, this);
+		return map;
+	}),
+
+	// Default template resolver, it should return documentFragment instance
 	// (this method may be overriden on subclasses for custom needs)
 	resolveTemplate: d(function (fn, context) { return fn.call(context); }),
 
+	// View validation function
+	// (this method may be overriden on subclasses for custom needs)
 	ensureView: d(function (conf) {
 		forEach(ensureObject(conf), function (value, key) {
 			var isConf;
