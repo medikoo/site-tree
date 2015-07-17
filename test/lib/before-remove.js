8e52eb18da49ef32a793260c642799af35d1871b
Mariusz Nowak <medyk@medikoo.com>
2015-07-17 09:33:05 +0200

    Delay removal of stylesheets from document
    It's needed for IE, which flickers when we remove and immediately
    add same stylesheet

---
+++ test/lib/before-remove.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (t, a) {
+	var div = document.createElement('div'), link;
+
+	div.appendChild(document.createElement('div'));
+	div.appendChild(document.createElement('div'));
+	div.appendChild(document.createElement('div'));
+	link = div.firstChild.appendChild(document.createElement('link'));
+	link.setAttribute('rel', 'stylesheet');
+	link.setAttribute('href', '/foo.css');
+
+	t(div.childNodes);
+};
