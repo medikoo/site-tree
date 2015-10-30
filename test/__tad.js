346f55ec25251b03c44fb12c0c736d5b1a0a79bc
Mariusz Nowak <medyk@medikoo.com>
2015-10-30 15:31:21 +0100

    Update to use latest jsdom

--- test/__tad.js
+++ test/__tad.js
@@ -1,20 +1,6 @@
 'use strict';
 
-var ClassList = require('class-list')
-  , document;
-
-document = require('jsdom').jsdom();
-if (document) {
-	Object.defineProperty(document.defaultView.HTMLElement.prototype, 'classList', {
-		get: function () {
-			Object.defineProperty(this, 'classList',
-				{ value: null, writable: true, configurable: true });
-			Object.defineProperty(this, 'classList',
-				{ value: new ClassList(this), writable: true, configurable: true });
-			return this.classList;
-		}
-	});
-}
+var document = require('jsdom').jsdom();
 
 exports.context = document ? {
 	document: document,
